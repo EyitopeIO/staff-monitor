372afa3abf6425a9feea9b23c1a32ed2577fa663
Eyitope Adelowo <topeboy2009@gmail.com>
2018-09-09 01:57:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,32 @@
-#include <functionDef2.h>
-#include <bluetooth.h>
-
-#ifndef REG52
 #include <REG52.h>
-#endif
+#include <common.h>
 
+#define NEXT //debug purposes only
+/*
+*** NOTES ***
+(1) Modem data format \r\n+PBREADY\r\n
+*/
 
+extern volatile bit RXDcmpt;
+extern volatile unsigned char num_CR_LF;
+extern volatile unsigned char rcvd_serial_data[RX_BUFFER_SIZE];
+extern volatile unsigned char occurred_cr_lf;
+unsigned char *progress;
 
-
-//modemInit would use these below. I'm yet to edit.
-unsigned char feb=1;
-bit fl=0,RXDcmpt;
-unsigned char SERIALcase= 1;
-unsigned char receivedData[50], count = 0;
-
-void main () {
+void main(){
+	*progress = 0x00; 
+	P1 = 0xFF; //step 0 
+	serialSetup('t');
+	serialAll("START"); //debug: see on serial monitor
+	while(occurred_cr_lf != 2); //wait for the closing \r\n
+	INC(&progress); //step 1
+	if(confirmData(rcvd_serial_data, STARTUP_RESP, strlen(STARTUP_RESP))){    //check for +PBREADY in the RX buffer
+		INC(progress); //step 2
+	}
+	P2 = 0xFF;
+	while(1){
+	}
+	//check for the array here
 	
-	/* initialise serial port
-	*  initialise modem
-	*  initialise bluetooth
-	*  wait for interrupt from PIR
-	*  tell bluetooth to scan when you get interrupt
-	*  send ID via text on modem
-	*  repeat
-	*/
 	
-	serialPortInit();
-	modemInit();
-	bluetoothInit();
 }
