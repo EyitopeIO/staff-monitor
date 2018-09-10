75cd934c03135756c4aafc063d1b9fe5f6f963c0
Eyitope Adelowo <topeboy2009@gmail.com>
2018-09-10 17:19:32 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,26 +7,37 @@
 (1) Modem data format \r\n+PBREADY\r\n
 */
 
-extern volatile bit RXDcmpt;
+extern volatile unsigned char RXDcmpt;
 extern volatile unsigned char num_CR_LF;
 extern volatile unsigned char rcvd_serial_data[RX_BUFFER_SIZE];
 extern volatile unsigned char occurred_cr_lf;
-unsigned char *progress;
+unsigned long count = 0;
 
 void main(){
-	*progress = 0x00; 
-	P1 = 0xFF; //step 0 
-	serialSetup('t');
-	serialAll("START"); //debug: see on serial monitor
-	while(occurred_cr_lf != 2); //wait for the closing \r\n
-	INC(&progress); //step 1
-	if(confirmData(rcvd_serial_data, STARTUP_RESP, strlen(STARTUP_RESP))){    //check for +PBREADY in the RX buffer
-		INC(progress); //step 2
-	}
+	P0 = 0xFF;
+	P1 = 0xFF;
 	P2 = 0xFF;
+	delay(2);
+	reset_serial_para(); 
+	serialSetup('t');
+	serialAll("START\r\n"); //debug: see on serial monitor
+	//while(occurred_cr_lf != 2); //wait for valid data
+	serialSetup('t');
+	serialAll("Just before checking data\r\n");
+	while(1){ //interrupt should increment this. Nothing happens!
+		count++;
+		if(confirmData(rcvd_serial_data,STARTUP_RESP,strlen(STARTUP_RESP))){
+			break;
+		}
+		if(count == 0xFFF){
+			count = 0;
+			serialAll("your number is: ");
+			send(occurred_cr_lf);
+			send('\n');
+		}
+	}
+	P2 = 0x00;
 	while(1){
 	}
-	//check for the array here
-	
-	
 }
+	
