94eb644ea2896596596e632d6963a83e3953a9e0
Eyitope Adelowo <topeboy2009@gmail.com>
2018-09-19 16:08:32 +0100

    Update main.c
    (1) Variable ble_or_modem added and will also tell how to control the multiplexer

--- main.c
+++ main.c
@@ -1,28 +1,38 @@
 #include <REG52.h>
 #include <common.h>
 
-#define NEXT //debug purposes only
+#
 /*
 *** NOTES ***
-(1) Modem data format \r\n+PBREADY\r\n
+(1) Modem data format \r\n<data>\r\n
+(2) Use the global variable ble_or_modem to indicate
+		MCU is working on the BLE device or MODEM. A multiplexer
+		would be built to work with this variable.
 */
 
 extern bit RXDcmpt;
+extern unsigned char ble_or_modem;
 extern volatile unsigned char rcvd_serial_data[RX_BUFFER_SIZE];
-extern volatile unsigned int occurred_cr_lf;
+extern volatile unsigned int got_cr_lf;
 extern volatile unsigned int index;
-unsigned long count = 0;
+		 
 
 void main(){
 	P0 = 0xFF;
 	P1 = 0xFF;
+	P2 = 0x00;
+	delay(5);
 	P2 = 0xFF;
-	delay(2); 
-	serialSetup('t');
+	serialSetup('t'); //set for baudrate = 9600
 	reset_serial_para();
-	modemSetup();
 	
+	ble_or_modem = 'm'; //set for modem
+	modemSetup((unsigned char)3); //repeat each command 3 times if failure
+	
+	ble_or_modem = 'b'; //set for bluetooth
+	bluetoothStart('i'); //inititialize bluetooth. 'i' for initialize
 	P0 = 0x00;
-	while(1);
+	
+	while(1); //Pending overall logic
 }
 	
