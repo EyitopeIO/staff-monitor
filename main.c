8c8d0161c173412b1e3bf82b3d10b5c147ab77bd
Eyitope Adelowo <topeboy2009@gmail.com>
2018-09-19 17:23:55 +0100

    Update main.c
    Delay is now in multiples of 71ms. I will change it to count in 1ms later.

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 		would be built to work with this variable.
 */
 
+extern bit PIR; //used to check status of PIR
 extern bit RXDcmpt;
 extern unsigned char ble_or_modem;
 extern volatile unsigned char rcvd_serial_data[RX_BUFFER_SIZE];
@@ -18,12 +19,12 @@ extern volatile unsigned int index;
 		 
 
 void main(){
-	P0 = 0xFF;
+	P0 = 0xFF; 
 	P1 = 0xFF;
 	P2 = 0x00;
-	delay(5);
-	P2 = 0xFF;
-	serialSetup('t'); //set for baudrate = 9600
+	delay(5); //parameter is multiples of 71ms. Use 14 for 1s
+	P2 = 0xFF; //This and above is debug
+	serialSetup('t'); //set timer for baudrate = 9600
 	reset_serial_para();
 	
 	ble_or_modem = 'm'; //set for modem
@@ -31,8 +32,15 @@ void main(){
 	
 	ble_or_modem = 'b'; //set for bluetooth
 	bluetoothStart('i'); //inititialize bluetooth. 'i' for initialize
+	//bluetoothStart('n');
 	P0 = 0x00;
 	
-	while(1); //Pending overall logic
+	while(1){
+		//turn on external interrupt for PIR here
+		P1 = 0x00;
+		delay(6);
+		P1 = 0xFF;
+		delay(14);
+	}
 }
 	
