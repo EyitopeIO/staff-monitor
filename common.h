06f3993a284cb75cd3c4e033c97729fe8f3075fe
Eyitope Adelowo <topeboy2009@gmail.com>
2018-09-27 11:07:58 +0100

    Update common.h

--- common.h
+++ common.h
@@ -3,14 +3,15 @@
 /*
 *** ALGORITHM ***
 (1) Setup modem and BLE
-(2) If PIR goes high, scan for bluetooth devices for as long as PIR is high, 
-		then send to server. For now, this server is just a person's phone.
-(3) Chill while PIR is low. 
+(2) PIR going high triggers interrupt that sets a flag. MCU polls the flag.
+(3) If high, turn off interrupt, scan, send to modem, resume. In future, 
+		transmission would be to some server
+
 
 Below is the format of the bluetooth data.
 
 %<address>,<type>,<RSSI>,Brcst:<data>%
-%DCF740B78604,1,C8,Brcst:0201041AFF590002150112233445566778899AABBCCDDEEFF040B78604BB%\r\n%FDE34...
+%DCF740B78604,1,C8,Brcst:0201041AFF590002150112233445566778899AABBCCDDEEFF040B78604BB%\r\n%BFF2140B33604,0,FF,Brcst:F2010317DA590002BA3812233445566778899AABB123AFEFF040B78604BB%
 
 *** SETTINGS ***
 baud rate = 9600
